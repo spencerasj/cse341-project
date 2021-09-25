603f07223b369dbb492f0a5d17beedf77dc04543
spencerasj <spe19021@byui.edu>
2021-09-25 16:54:23 -0600

    added in prove02 to nav bar

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-/*******************************************************************************
- * Feel free to remove this comment block and all other comments after pulling.
- * They're for information purposes only.
- *
- * This layout is provided to you for an easy and quick setup to either pull
- * or use to correct yours after working at least 1 hour on Team Activity 02.
- * Throughout the course, we'll be using Express.js for our view engines.
- * However, feel free to use pug or handlebars ('with extension hbs'). You will
- * need to make sure you install them beforehand according to the reading from
- * Udemy course.
+/*
  * IMPORTANT: Make sure to run "npm install" in your root before "npm start"
  *******************************************************************************/
 // Our initial setup (package requires, port number setup)
@@ -24,20 +15,26 @@ const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
 
+//Routes for prove 02 assignments
+const adminData = require('./prove02/routes/admin');
+const shopRoutes = require('./prove02/routes/shop');
+
 app
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join(__dirname, 'prove02/public')))
   .set('view engine', 'ejs')
-  // For view engine as Pug
-  //.set('view engine', 'pug') // For view engine as PUG.
-  // For view engine as hbs (Handlebars)
-  //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
-  //.set('view engine', 'hbs')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
+
+  .set('views', [__dirname + '/views', __dirname + '/prove02/views'])
+
+  .use(bodyParser({
+    extended: false
+  })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
+  .use('/admin', adminData.routes)
+  .use(shopRoutes)
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
     res.render('pages/index', {
@@ -45,8 +42,12 @@ app
       path: '/',
     });
   })
+
   .use((req, res, next) => {
     // 404 page
-    res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
+    res.render('pages/404', {
+      title: '404 - Page Not Found',
+      path: req.url
+    });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
--- prove02/app.js
+++ prove02/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
